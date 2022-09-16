0d385a312c79641506bcca65431f193108dcdfc0
JidapaBubu <jidapa4499@gmail.com>
2022-09-16 12:24:41 +0700

    test

--- Program.cs
+++ Program.cs
@@ -19,10 +19,27 @@ namespace MDTwork1023
             if (f == 1)
             {
                 Console.WriteLine("Register Values : 1,000,000 Bath");
-                Console.WriteLine("Shop Brand : KFC Bang Khun Thian Seaside ");
-                Console.WriteLine("Founder Name : A ");
+                Console.WriteLine("Shop Brand : KFC Bang Khun Thian Seaside");
             }
 
+            else if (f == 2)
+            {
+                Console.WriteLine("Founders Name : Asaa Surname : Radee Id: 11048-24597-744");
+                Console.WriteLine("Founders Name : Saaa Surname : TT Id: 12450-24597-888");
+                Console.WriteLine("Founders Name : Goo Surname : Tyyy Id: 11048-45966-659");
+            }
+            else if (f == 3)
+            {
+                Console.WriteLine("**********Salay Worker*********");
+                Console.WriteLine("Name: JJ Surname: UU Age: 20 Salary: 15,000 bath");
+                Console.WriteLine("Name: Wera Surname: Deerek Age: 26 Salary: 15,000 bath");
+                Console.WriteLine("Name: Doo Surname: Rattee Age: 22 Salary: 15,000 bath");
+                Console.WriteLine();
+                Console.WriteLine("**********Delivery************");
+                Console.WriteLine("Name: Ruu Surname: Saee Age: 20 Salary: 12,500 bath");
+                Console.WriteLine("Name: Firy Surname: Isches Age: 19 Salary: 12,500 bath");
+            }
+        
     
 
         }
